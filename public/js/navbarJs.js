2b29521b952c46c41e54efdc87a1c51c7abfcb71
user <user@gmail.com>
2020-02-12 06:48:48 +0530

    buy now button and a tag solve when toggle open

--- public/js/navbarJs.js
+++ public/js/navbarJs.js
@@ -1,23 +1,30 @@
-
+		var navMeuButton = document.getElementsByClassName('menu-bar')[0]
     	var toggle = document.getElementById('toggle')
 		var toggleMenu = document.getElementById('toggle-menu')
-		var toggleMenuButton = document.getElementById('toggle-menu-button')
-		var toggleCloseButton = document.getElementById('toggle-close')
+		var toggleMenuButton = document.getElementById('toggle-menu-button') 
+		var toggleCloseButton = document.getElementById('toggle-close') 
 		var shopName = document.getElementsByClassName('shop-name')[0]
 		var leftMenuBar = document.getElementsByClassName('left-menu')[0]
 		var searchSuggestion = document.getElementById('search-suggestion')
 		var searchInput = document.getElementById('search-input')
 		var closeButton = document.getElementById('close-icon')
 		var searchSuggestionList = document.getElementsByClassName('search-suggestion-li')
+		
+
+		
 
 		toggleMenuButton.addEventListener('click', function(event) {
 					toggle.style.display = 'none';
-    				leftMenuBar.style.display = 'block'
+					leftMenuBar.style.display = 'block'
+
+					
 		})
 
 		toggleCloseButton.addEventListener('click', function(event) {
 					toggle.style.display = 'none';
 					leftMenuBar.style.display = 'block'
+
+					
 		})
 
     	window.addEventListener('mouseup', function(event) {
@@ -25,9 +32,10 @@
     			
     		 		if( event.target != toggleMenu ) {
     		 		toggle.style.display = 'none';
-    		 		leftMenuBar.style.display = 'block'
-    		 		
-    		 	}   
+					 leftMenuBar.style.display = 'block'
+					   		 		
+				 }   
+		
 
         })
 
